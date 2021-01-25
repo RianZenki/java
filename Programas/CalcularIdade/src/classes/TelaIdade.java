a36851f0ee55ac8ab692a7ec95496bea6079750a
Rian Zenki Nacazato <rian.zenki@gmail.com>
2021-01-25 05:23:57 -0300

    Exercicios

--- Programas/CalcularIdade/src/classes/TelaIdade.java
+++ Programas/CalcularIdade/src/classes/TelaIdade.java
@@ -139,6 +139,9 @@ public class TelaIdade extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(TelaIdade.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
